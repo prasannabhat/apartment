aebe0783caa9cc3c7092928a994ee3fc86e3ad46
prasannabhat <prasanna.yoga@gmail.com>
2012-10-06 10:26:20 +0530

    Fix issue in collapsing layout.
    Custom style was overriding bootstrap style.Removed the custom css

--- application/views/layouts/main.blade.php
+++ application/views/layouts/main.blade.php
@@ -28,6 +28,11 @@
     <div class="navbar navbar-inverse navbar-fixed-top">
       <div class="navbar-inner">
         <div class="container-fluid">
+           <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
           <span class="brand">Apartment Management</span>
           <div class="nav-collapse collapse">
             <p class="navbar-text pull-right">
