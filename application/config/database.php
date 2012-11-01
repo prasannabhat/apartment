479df30228cc20d17d978104d78c70e5279f2678
prasannabhat <prasanna.yoga@gmail.com>
2012-11-01 20:38:43 +0530

    test configurations only

--- application/config/database.php
+++ application/config/database.php
@@ -70,7 +70,7 @@ return array(
 		'mysql' => array(
 			'driver'   => 'mysql',
 			'host'     => 'localhost',
-			'database' => 'apartment',
+			'database' => 'apartmenttest',
 			'username' => 'root',
 			'password' => '',
 			'charset'  => 'utf8',
