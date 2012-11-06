0c824aa22096a584b64e4a541aff6e446847be7b
Prasanna Bhat <prasanna.bhat>
2012-11-06 12:33:40 +0530

    restrict certain file access, and block access to all subfolders.

--- application/tasks/dbbackup.php
+++ application/tasks/dbbackup.php
@@ -56,7 +56,7 @@ class dbbackup_Task {
 		        echo 'Database <b>' .$mysqlDatabaseName .'</b> successfully exported to <b>~/' . $file_path .'</b>';
 		        break;
 		    case 1:
-		        echo 'There was a warning during the export of <b>' .$mysqlDatabaseName .'</b> to <b>~/' .$mysqlExportPath .'</b>';
+		        echo 'There was a warning during the export of <b>' .$mysqlDatabaseName .'</b> to <b>~/' .$file_path .'</b>';
 		        break;
 		    case 2:
 		        echo 'There was an error during export. Please check your values:<br/><br/><table><tr><td>MySQL Database Name:</td><td><b>' .$mysqlDatabaseName .'</b></td></tr><tr><td>MySQL User Name:</td><td><b>' .$mysqlUserName .'</b></td></tr><tr><td>MySQL Password:</td><td><b>NOTSHOWN</b></td></tr><tr><td>MySQL Host Name:</td><td><b>' .$mysqlHostName .'</b></td></tr></table>';
@@ -87,7 +87,7 @@ class dbbackup_Task {
 		        echo 'Import file <b>' .$file_path .'</b> successfully imported to database <b>' . $mysqlDatabaseName .'</b>';
 		        break;
 		    case 1:
-		        echo 'There was an error during import. Please make sure the import file is saved in the same folder as this script and check your values:<br/><br/><table><tr><td>MySQL Database Name:</td><td><b>' .$mysqlDatabaseName .'</b></td></tr><tr><td>MySQL User Name:</td><td><b>' .$mysqlUserName .'</b></td></tr><tr><td>MySQL Password:</td><td><b>NOTSHOWN</b></td></tr><tr><td>MySQL Host Name:</td><td><b>' .$mysqlHostName .'</b></td></tr><tr><td>MySQL Import Filename:</td><td><b>' .$mysqlImportFilename .'</b></td></tr></table>';
+		        echo 'There was an error during import. Please make sure the import file is saved in the same folder as this script and check your values:<br/><br/><table><tr><td>MySQL Database Name:</td><td><b>' .$mysqlDatabaseName .'</b></td></tr><tr><td>MySQL User Name:</td><td><b>' .$mysqlUserName .'</b></td></tr><tr><td>MySQL Password:</td><td><b>NOTSHOWN</b></td></tr><tr><td>MySQL Host Name:</td><td><b>' .$mysqlHostName .'</b></td></tr><tr><td>MySQL Import Filename:</td><td><b>' .$file_path .'</b></td></tr></table>';
 		        break;
 		}		
 		
