2c6e137ae76714851ec721c979488ab8d19239b0
prasannabhat <prasanna.yoga@gmail.com>
2012-11-11 23:50:19 +0530

    remove sms gateway info from staging configuration

--- application/config/staging/apartment.php
+++ application/config/staging/apartment.php
@@ -3,22 +3,6 @@
 return array(
 //Custom configuration for apartment related details. 
 
-	'sms_gateways' => array(
-		'way2sms' => array(
-				'login' => '8147256460',
-				'password' => 'amrutaprasanna'
-			),
-		'160by2' => array(
-				'login' => '8147256460',
-				'password' => 'amrutaprasanna'
-			),
-		'fullonsms' => array(
-				'login' => '8147256460',
-				'password' => 'amrutaprasanna'
-			),			
-						
-	),
-
 	'test_no' => '8147256460',
 
 	'allowed_gateways' => array('way2sms','160by2','fullonsms'),
